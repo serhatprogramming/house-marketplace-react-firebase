8e7d44296a69d483dc5129995db317eed60bdf2f
serhatprogramming <serhatprogramming@gmail.com>
2022-08-25 15:02:50 -0400

    save user to db on firebase

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -8,6 +8,7 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { db } from "../firebase.config";
+import { doc, setDoc, serverTimestamp } from "firebase/firestore";
 
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
@@ -33,16 +34,17 @@ function SignUp() {
 
     try {
       const auth = getAuth();
-
       const userCredential = await createUserWithEmailAndPassword(
         auth,
         email,
         password
       );
       const user = userCredential.user;
-
+      const formDataCopy = { ...formData };
+      delete formDataCopy.password;
+      formDataCopy.timestamp = serverTimestamp();
+      await setDoc(doc(db, "users", user.uid), formDataCopy);
       updateProfile(auth.currentUser, { displayName: name });
-
       navigate("/");
     } catch (error) {
       console.log(error);
