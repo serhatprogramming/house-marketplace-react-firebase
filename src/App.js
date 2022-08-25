c7d5e72c769fc5d6714cdb3934a21b86e0e5ee5d
serhatprogramming <serhatprogramming@gmail.com>
2022-08-25 12:23:29 -0400

    pages routes added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ForgotPassword from "./pages/ForgotPassword";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 function App() {
   return (
     <>
-      <h1>My App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<SignIn />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </BrowserRouter>
+      {/* navbar */}
     </>
   );
 }
