9b1a2292ec93cbe9af14980257e040799f6f8c05
Grant <github@firefishy.com>
2022-08-28 01:10:30 +0300

    Update Listing.js
    `{s}.` is no longer recommended now that we support HTTP/2 + HTTP/3.

--- src/pages/Listing.js
+++ src/pages/Listing.js
@@ -127,7 +127,7 @@ function Listing() {
           >
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             <Marker
               position={[listing.geolocation.lat, listing.geolocation.lng]}
